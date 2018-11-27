29755d81fd1a9df0f3fffd3b843b97e4c12fce57
Stephen B Bamwite <40039818+RonKbS@users.noreply.github.com>
2018-11-27 11:03:25 +0300

    chore(setup): setup sass and css framework (#4)
    -install node-sass
    -add cdn to index.html
    
    [Delivers #162190940]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from '../components/notFound/NotFound';
+import LandingPage from '../components/landingPage/LandingPage';
 
 const Routes = () => (
   <Switch>
+    <Route exact path="/" component={LandingPage} />
     <Route exact component={NotFound} />
   </Switch>
 );
