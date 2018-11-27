e884944dbc3fa918d7833f00d2f11ed564e0d66b
RonKbS <stephen.bamwite@gmail.com>
2018-11-27 09:49:36 +0300

    chore(setup): setup routing
    -install react-router-dom
    -create a route to page not found
    
    [Delivers #162193617]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/index';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-   To get started, edit
-      <code>src/App.js</code>
-   and save to reload
-    </p>
-  </div>
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <h4>MyDiary did not find the page you are looking for</h4>;
+
+export default NotFound;
